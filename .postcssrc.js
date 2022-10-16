46e0a3bc2b76512573d56e7c8a9b7af2220493fd
wjdwlcks <ah3851wen@naver.com>
2022-10-16 22:09:05 +0900

    gd

---
+++ .postcssrc.js
@@ -0,0 +1,15 @@
+//ESM
+//CommonJS
+
+//import autoprefixer from 'autoprefixer'
+//const autoprefixer = require('autoprefixer')
+
+
+
+//export
+module.exports = {
+    plugins: [
+        //autoprefixer
+        require('autoprefixer')
+    ]
+}
---
+++ js/main.js
@@ -0,0 +1,9 @@
+console.log('hg');
+
+
+async function test () {
+    const promise = Promise.resolve(123)
+    console.log(await promise)
+}
+
+test()
